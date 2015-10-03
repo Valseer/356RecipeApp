1259cb7618a9e11216cc1503f46e91b15456b618
Tyler Gerber <tcgerber10567@gmail.com>
2015-10-03 15:34:43 -0400

    Beginning of creating the view. still issues with manifest XML

---
+++ RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/RecipeView.java
@@ -0,0 +1,15 @@
+package com.example.tyler_000.recipeapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.R;
+
+public class RecipeView extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_view);
+    }
+
+}
