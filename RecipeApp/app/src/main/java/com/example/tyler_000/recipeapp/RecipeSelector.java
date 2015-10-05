3fdbf73488f6442f06bab248a2c4d8b0ca6173a6
Tyler Gerber <tcgerber10567@gmail.com>
2015-10-05 14:41:00 -0400

    more basic XML stoofs

---
+++ RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/RecipeSelector.java
@@ -0,0 +1,14 @@
+package com.example.tyler_000.recipeapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RecipeSelector extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_selector);
+    }
+
+}
