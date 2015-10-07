3db7b199e875a59405eb374501575489874d095e
John Sellers <jrs9025@rit.edu>
2015-10-07 14:54:53 -0400

    Changes to the Recipe class for naming and variables.

---
+++ RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/Parser.java
@@ -0,0 +1,10 @@
+package com.example.tyler_000.recipeapp;
+
+/**
+ * Created by john on 10/7/2015.
+ */
+public class Parser {
+    public static Recipe parseRecipe(String fileLocation){
+        return null;
+    }
+}
--- RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/recipe.java
+++ RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/recipe.java
@@ -1,7 +1,47 @@
 package com.example.tyler_000.recipeapp;
 
+import java.util.HashMap;
+
 /**
  * Created by john on 10/5/2015.
  */
-public class recipe {
+
+
+
+public class Recipe {
+    protected String recipeTitle;
+    //protected Step firstStep;
+    protected HashMap ingredients;
+
+    public Recipe(){
+
+    }
+
+    public Recipe(String fileLocation){
+        Recipe rec= Parser.parseRecipe(fileLocation);
+    }
+
+    public void displayRecipe(){
+
+    }
+
+    /*public Step startRecipe(){
+        return null;
+    }*/
+
+    public String getRecipeTitle(){
+        return recipeTitle;
+    }
+    public void setRecipeTitle(String title){
+        recipeTitle=title;
+    }
+
+    public HashMap getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(HashMap ingredients) {
+        this.ingredients = ingredients;
+    }
+
 }
