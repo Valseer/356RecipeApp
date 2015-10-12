6aaaf0310fa3e077384efc8e652acb2753d7ccd1
Tyler Gerber <tcgerber10567@gmail.com>
2015-10-12 16:50:51 -0400

    Maybe actual list view?

--- RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/Recipe_Selector.java
+++ RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/Recipe_Selector.java
@@ -1,33 +1,35 @@
 package com.example.tyler_000.recipeapp;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.text.Layout;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class Recipe_Selector extends Activity {
+public class Recipe_Selector extends ListActivity {
 ListView myLayout;
 ArrayList<Recipe> recipeList;
+    ArrayList<String> recipeNames = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe__selector);
-        myLayout = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,recipeNames);
         recipeList.add(new Recipe());
         recipeList.add(new Recipe());
         recipeList.add(new Recipe());
+        setListAdapter(adapter);
         for(int i=0; i < recipeList.size(); i++) {
             recipeList.get(i).setRecipeTitle("Potatoes" + i);
-            Button b = new Button(this);
-            b.setHeight(20);
-            b.setWidth(myLayout.getWidth());
-            b.setText(recipeList.get(i).getRecipeTitle());
-            myLayout.addView(b);
+            recipeNames.add(recipeList.get(i).getRecipeTitle());
+            adapter.notifyDataSetChanged();
         }
     }
 
