6c5501ce2c97fa28004ec5246905eb330061ebf4
Tyler Gerber <tcgerber10567@gmail.com>
2015-10-12 16:07:17 -0400

    no message

--- RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/Recipe_Selector.java
+++ RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/Recipe_Selector.java
@@ -11,16 +11,16 @@ import java.util.ArrayList;
 
 public class Recipe_Selector extends Activity {
 ListView myLayout;
-ArrayList<recipe> recipeList;
+ArrayList<Recipe> recipeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe__selector);
         myLayout = (ListView) findViewById(R.id.listView);
-        recipeList.add(new recipe());
-        recipeList.add(new recipe());
-        recipeList.add(new recipe());
+        recipeList.add(new Recipe());
+        recipeList.add(new Recipe());
+        recipeList.add(new Recipe());
         for(int i=0; i < recipeList.size(); i++) {
             Button b = new Button(this);
             b.setHeight(20);
