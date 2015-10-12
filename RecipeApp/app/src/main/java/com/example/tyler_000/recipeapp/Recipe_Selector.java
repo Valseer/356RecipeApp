167955a97cd786f17f3dc6b61ecec8e1a3af82fa
John Sellers <jrs9025@rit.edu>
2015-10-12 17:43:08 -0400

    Pushing parcelable update for recipe and step class

--- RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/Recipe_Selector.java
+++ RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/Recipe_Selector.java
@@ -2,8 +2,6 @@ package com.example.tyler_000.recipeapp;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.text.Layout;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -11,16 +9,16 @@ import java.util.ArrayList;
 
 public class Recipe_Selector extends Activity {
 ListView myLayout;
-ArrayList<recipe> recipeList;
+ArrayList<Recipe> recipeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe__selector);
         myLayout = (ListView) findViewById(R.id.listView);
-        recipeList.add(new recipe());
-        recipeList.add(new recipe());
-        recipeList.add(new recipe());
+        recipeList.add(new Recipe());
+        recipeList.add(new Recipe());
+        recipeList.add(new Recipe());
         for(int i=0; i < recipeList.size(); i++) {
             Button b = new Button(this);
             b.setHeight(20);
--- RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/recipe.java
+++ RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/recipe.java
@@ -1,6 +1,15 @@
 package com.example.tyler_000.recipeapp;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.lang.reflect.ParameterizedType;
+import java.security.SecureClassLoader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * Created by john on 10/5/2015.
@@ -8,10 +17,11 @@ import java.util.HashMap;
 
 
 
-public class Recipe {
+public class Recipe implements Parcelable {
     protected String recipeTitle;
-    //protected Step firstStep;
+    protected Step firstStep;
     protected HashMap ingredients;
+    protected ArrayList<Step> recipeSteps;
 
     public Recipe(){
 
@@ -25,9 +35,10 @@ public class Recipe {
 
     }
 
-    /*public Step startRecipe(){
-        return null;
-    }*/
+    public Step startRecipe(){
+        return recipeSteps.get(0);
+    }
+
 
     public String getRecipeTitle(){
         return recipeTitle;
@@ -44,4 +55,47 @@ public class Recipe {
         this.ingredients = ingredients;
     }
 
+
+    protected Recipe(Parcel in) {
+        recipeTitle = in.readString();
+        firstStep = (Step) in.readValue(Step.class.getClassLoader());
+        ingredients = (HashMap) in.readValue(HashMap.class.getClassLoader());
+        if (in.readByte() == 0x01) {
+            recipeSteps = new ArrayList<Step>();
+            in.readList(recipeSteps, Step.class.getClassLoader());
+        } else {
+            recipeSteps = null;
+        }
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(recipeTitle);
+        dest.writeValue(firstStep);
+        dest.writeValue(ingredients);
+        if (recipeSteps == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeList(recipeSteps);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Recipe> CREATOR = new Parcelable.Creator<Recipe>() {
+        @Override
+        public Recipe createFromParcel(Parcel in) {
+            return new Recipe(in);
+        }
+
+        @Override
+        public Recipe[] newArray(int size) {
+            return new Recipe[size];
+        }
+    };
 }
