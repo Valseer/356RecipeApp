259734366b35cc601ee26a63d4e3313383698fbf
Jean Cheng <jxc3889@rit.edu>
2015-10-12 17:04:08 -0400

    Added functions for Liquid, Mass, and Volume Conversion
    Returns double (input is double)

---
+++ RecipeApp/app/src/main/java/com/example/tyler_000/recipeapp/MassConvert.java
@@ -0,0 +1,32 @@
+package com.example.tyler_000.recipeapp;
+
+/**
+ * Created by Jean on 10/12/2015.
+ */
+public class MassConvert {
+    //pound(lb)  1 lb = 16 oz
+    //ounce(oz)
+
+/**
+ * POUND
+ */
+
+    /**
+     * pound to Ounces
+     **/
+    public double pound_to_Ounces(double pound )
+    {
+        return (pound * 16);
+    }
+
+/**
+ * OUNCES
+ */
+    /**
+     * ounce (oz) to pound
+     **/
+    public double Ounce_to_pound(double Ounce )
+    {
+        return (Ounce * 0.0625);
+    }
+}
